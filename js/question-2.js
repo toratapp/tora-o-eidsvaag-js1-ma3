061d8c7e51ffa5d766964a4fd88b950d4a285827
toratapp <73744488+toratapp@users.noreply.github.com>
2021-10-02 17:17:10 +0200

    Working on Q2

--- js/question-2.js
+++ js/question-2.js
@@ -2,6 +2,8 @@
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=969d394c9a3f4fe2b95c2ebf4ec52d64";
 
+const descriptionsContainer = document.querySelector(".descriptions");
+
 async function getGames() {
 
     const response = await fetch(url);
@@ -10,8 +12,21 @@ async function getGames() {
 
     const descriptions = results.results;
 
+    // console.log(descriptions);
+
+    // descriptionsContainer.innerHTML = "";
+
     for(let i = 0; i < descriptions.length; i++) {
-        console.log(descriptions[i].name);
+
+        console.log(`Name: ${descriptions[i].name}`);
+        console.log(`Rating: ${descriptions[i].rating}`);
+        console.log(`Amount of tags: ${descriptions[i].tags.length}`);
+
+        if(i === 7) {
+            break;
+        }
+
+        // descriptionsContainer.innerHTML += "";
     }
 
 }
